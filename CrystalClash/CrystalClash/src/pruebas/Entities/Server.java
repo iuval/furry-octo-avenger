e49b6ab14c8ce0258a1da7e2d5fda36c7c6f1ae3
iuval <iuvalgoldansky@gmail.com>
2013-07-22 20:28:34 -0300

    borre Server y cree Networking/ServerDriver

--- CrystalClash/CrystalClash/src/pruebas/Entities/Server.java
+++
@@ -1,8 +0,0 @@
-package pruebas.Entities;
-
-public class Server {
-
-	public static void getGames(User user){
-		//Pide al servidor la lista de partidas del usuario recibido
-	}
-}
---
+++ CrystalClash/CrystalClash/src/pruebas/Networking/ServerDriver.java
@@ -0,0 +1,56 @@
+package pruebas.Networking;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import pruebas.Entities.MenuLogIn;
+import pruebas.Entities.MenuMaster;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Net.HttpMethods;
+import com.badlogic.gdx.Net.HttpRequest;
+import com.badlogic.gdx.Net.HttpResponse;
+import com.badlogic.gdx.Net.HttpResponseListener;
+import com.badlogic.gdx.net.HttpParametersUtils;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+
+public class ServerDriver {
+	private static String SERVER_URL = "http://fuzzy-adventure.herokuapp.com/";
+
+	public static void SignIn(String email, String password){
+		Map<String, String> data = new HashMap<String, String>();
+		data.put("email", "email");
+		Post("sign_in",data);
+	}
+	
+	public static void LogIn(String email, String password){
+		Map<String, String> data = new HashMap<String, String>();
+		data.put("email", email);
+		Post("log_in",data);
+	}
+
+	private static void Post(String action, Map<String, String> data ) {
+		HttpRequest httpPost = new HttpRequest(HttpMethods.POST);
+		httpPost.setUrl(SERVER_URL + action);
+		httpPost.setContent(HttpParametersUtils
+				.convertHttpParameters(data));
+
+		Gdx.net.sendHttpRequest(httpPost, new HttpResponseListener() {
+			public void handleHttpResponse(HttpResponse httpResponse) {
+				ProcessResponce(httpResponse.getResultAsString());
+			}
+
+			public void failed(Throwable t) {
+				String status = "failed";
+				System.out.println("failed");
+				// do stuff here based on the failed attempt
+			}
+		});
+	}
+
+	private static void ProcessResponce(String response) {
+		JsonValue root = new JsonReader().parse(response);
+	//	MenuMaster.getMenuLogIn().logInCallback(response);
+	}
+}
