eb5054cbd9c2000f84fc65e6db133571e6e6abb9
Sebastian <sebastianleonardi14@gmail.com>
2013-08-03 20:26:36 -0300

    Agregue los 2 botones (send y more options) Cuando hacemos click en more options muestra las otros 3 botones que siempre deben estar en el menu...

--- CrystalClash/CrystalClash/src/pruebas/Renders/GameEngine.java
+++ CrystalClash/CrystalClash/src/pruebas/Renders/GameEngine.java
@@ -8,6 +8,7 @@ import pruebas.Enumerators.GameState;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -67,7 +68,9 @@ public class GameEngine implements Screen {
 	}
 
 	private void renderGame(float dt) {
-
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		switch (state) {
 		case InGame:
 			batch.begin();
--- CrystalClash/CrystalClash/src/pruebas/Renders/WorldRender.java
+++ CrystalClash/CrystalClash/src/pruebas/Renders/WorldRender.java
@@ -1,34 +1,50 @@
 package pruebas.Renders;
 
-import java.util.Iterator;
-
+import pruebas.Accessors.ActorAccessor;
 import pruebas.Controllers.WorldController;
 import pruebas.CrystalClash.CrystalClash;
-import pruebas.Entities.Unit;
 import pruebas.Renders.helpers.CellHelper;
 import pruebas.Renders.helpers.UnitHelper;
-import pruebas.Util.Tuple;
+
+import aurelienribon.tweenengine.Timeline;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenManager;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class WorldRender implements InputProcessor {
 
 	private GameEngine engine;
+	private TweenManager tweenManager;
 	public static CellHelper cellHelper;
 
 	private Texture backgroundTexture;
 	private Image background;
+	private Image sendBar;
+	private Image moreOptions;
+	private Image optionsBar;
 
-	private Texture archerTexture;
-	private Image archer;
+	private TextButton btnSend;
+	private TextButton btnMoreOptions;
+	private TextButton btnSurrender;
+	private TextButton btnBack;
+	private TextButton btnClear;
+	private Group grpMoreOptions;
 
 	private WorldController world;
 	GameRender gameRender;
@@ -39,42 +55,162 @@ public class WorldRender implements InputProcessor {
 		this.engine = engine;
 		this.world = world;
 
+		tweenManager = new TweenManager();
+		
 		cellHelper = new CellHelper();
 		cellHelper.load();
 
 		UnitHelper.init();
+		gameRender = new SelectUnitsRender(engine, world, 2);
+		loadStuff();
+	}
+
+	public void render(float dt, SpriteBatch batch, Stage stage) {
+		background.draw(batch, 1);
+		for (int i = 0; i < world.cellGrid.length; i++) {
+			for (int j = world.cellGrid[i].length - 1; j >= 0; j--) {
+				world.cellGrid[i][j].getRender().draw(dt, batch);
+				world.cellGrid[i][j].getRender().drawUnits(dt, batch);
+			}
+		}
+
+		for (int i = 0; i < world.cellGrid.length; i++) {
+			for (int j = world.cellGrid[i].length - 1; j >= 0; j--) {
+			}
+		}
+
+		gameRender.render(dt, batch);
+
+		moreOptions.draw(batch, 1);
+		optionsBar.draw(batch, 1);
+
+		
+		stage.addActor(btnMoreOptions);
+		stage.addActor(grpMoreOptions);
+		stage.addActor(sendBar);
+		stage.addActor(btnSend);
+		grpMoreOptions.act(dt);
+		tweenManager.update(dt);
+	}
+
+	private void loadStuff() {
+		TextureAtlas atlas = new TextureAtlas(
+				"data/Images/InGame/options_bar.pack");
+		Skin skin = new Skin(atlas);
 
 		backgroundTexture = new Texture(
 				Gdx.files.internal("data/Images/InGame/terrain.jpg"));
 		background = new Image(backgroundTexture);
 		background.setSize(CrystalClash.WIDTH, CrystalClash.HEIGHT);
 
-		gameRender = new SelectUnitsRender(engine, world, 2);
+		TextureRegion aux = skin.getRegion("option_send_bar");
+		sendBar = new Image(aux);
+		sendBar.setPosition(0, 0);
+		aux = skin.getRegion("option_more_bar");
+		moreOptions = new Image(aux);
+		moreOptions.setPosition(sendBar.getWidth() - 35, 0);
+		aux = skin.getRegion("options_bar");
+		optionsBar = new Image(aux);
+		optionsBar.setPosition(0 - optionsBar.getWidth() - 75, 0);
+
+		BitmapFont font = new BitmapFont(
+				Gdx.files.internal("data/Fonts/font.fnt"), false);
+		TextButtonStyle sendStyle = new TextButtonStyle(
+				skin.getDrawable("option_send_button"),
+				skin.getDrawable("option_send_button_pressed"), null, font);
+		btnSend = new TextButton("", sendStyle);
+		btnSend.setPosition(0, 0);
+		btnSend.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				// TODO: Mandar al server los datos
+			}
+		});
+
+		TextButtonStyle moreStyle = new TextButtonStyle(
+				skin.getDrawable("option_more_button"),
+				skin.getDrawable("option_more_button_pressed"), null, font);
+		btnMoreOptions = new TextButton("", moreStyle);
+		btnMoreOptions.setPosition(moreOptions.getX() + moreOptions.getWidth()
+				- btnMoreOptions.getWidth(), 0);
+		btnMoreOptions.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				showMoreOptions();
+			}
+		});
+
+		TextButtonStyle optionsStyle = new TextButtonStyle(
+				skin.getDrawable("option_button"),
+				skin.getDrawable("option_button_pressed"), null, font);
+
+		btnSurrender = new TextButton("Surrender", optionsStyle);
+		btnSurrender.setPosition(0, 0);
+		btnSurrender.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				// TODO: Surrender
+			}
+		});
+
+		btnBack = new TextButton("Back to Menu", optionsStyle);
+		btnBack.setPosition(btnSurrender.getX() + btnSurrender.getWidth() + 2,
+				0);
+		btnBack.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				// TODO: back to menu
+			}
+		});
+
+		btnClear = new TextButton("Clear Moves", optionsStyle);
+		btnClear.setPosition(btnBack.getX() + btnBack.getWidth() + 2, 0);
+		btnClear.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				// TODO: clear moves
+			}
+		});
+
+		grpMoreOptions = new Group();
+		grpMoreOptions.addActor(btnSurrender);
+		grpMoreOptions.addActor(btnBack);
+		grpMoreOptions.addActor(btnClear);
+
+		grpMoreOptions.setSize(optionsBar.getWidth(), optionsBar.getHeight());
+		grpMoreOptions.setPosition(0 - grpMoreOptions.getWidth(),
+				optionsBar.getY() + 5);
 	}
 
-	public void render(float dt, SpriteBatch batch, Stage stage) {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		background.draw(batch, 1);
-		for (int i = 0; i < world.cellGrid.length; i++) {
-			for (int j = world.cellGrid[i].length - 1; j >= 0; j--) {
-				world.cellGrid[i][j].getRender().draw(dt, batch);
-			}
-		}
-
-		for (int i = 0; i < world.cellGrid.length; i++) {
-			for (int j = world.cellGrid[i].length - 1; j >= 0; j--) {
-				world.cellGrid[i][j].getRender().drawUnits(dt, batch);
-			}
-		}
-
-		gameRender.render(dt, batch);
+	private void showMoreOptions() {
+		float speed = 0.2f; //CrystalClash.ANIMATION_SPEED;
+		Timeline.createParallel()
+				.beginParallel()
+				.push(Tween.to(moreOptions, ActorAccessor.X, speed).target(0 - moreOptions.getWidth()))
+				.push(Tween.to(btnMoreOptions, ActorAccessor.X, speed).target(0 - btnMoreOptions.getWidth()))
+				.end().beginParallel().beginSequence()
+				.push(Tween.to(optionsBar, ActorAccessor.X, speed).target(sendBar.getWidth() - 70 + 15))
+				.push(Tween.to(optionsBar, ActorAccessor.X, 0.05f).target(sendBar.getWidth() - 70)).end()
+				.beginSequence()
+				.push(Tween.to(grpMoreOptions, ActorAccessor.X, speed).target(sendBar.getWidth() - 70 + 75 + 15))
+				.push(Tween.to(grpMoreOptions, ActorAccessor.X, 0.05f).target(sendBar.getWidth() - 70 + 75)).end()
+				.end().start(tweenManager);
+	}
+	
+	private void hideMoreOptions() {
+		float speed = 0.2f; //CrystalClash.ANIMATION_SPEED;
+		Timeline.createParallel()
+				.beginParallel()
+				.push(Tween.to(moreOptions, ActorAccessor.X, speed).target(sendBar.getWidth() - 35))
+				.push(Tween.to(btnMoreOptions, ActorAccessor.X, speed).target(sendBar.getWidth() - 35 + moreOptions.getWidth() - btnMoreOptions.getWidth()))
+				.end().beginParallel()
+				.push(Tween.to(optionsBar, ActorAccessor.X, speed).target(0 - optionsBar.getWidth()))
+				.push(Tween.to(grpMoreOptions, ActorAccessor.X, speed).target(0 - grpMoreOptions.getWidth()))
+				.end().start(tweenManager);
 	}
 
 	public void dispose() {
 		backgroundTexture.dispose();
-		archerTexture.dispose();
 	}
 
 	@Override
@@ -97,6 +233,7 @@ public class WorldRender implements InputProcessor {
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		hideMoreOptions();
 		Vector2 vec = engine.getRealPosition(screenX, screenY);
 		gameRender.touchDown(vec.x, vec.y, pointer, button);
 		return false;
