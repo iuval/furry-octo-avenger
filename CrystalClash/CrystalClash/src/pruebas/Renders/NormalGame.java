2fdbc6c195216d80db2349a6c42947d1c6d50d70
Sebastian <sebastianleonardi14@gmail.com>
2013-08-24 16:01:34 -0300

    Borre codigo comentado de las unidades de prueba. Arregle el metodo ShowAbleToMove para que te permita moverte si empezar el turno estando en una celda compartida (Antes solo chequeaba si habia una unidad enemiga, y en caso positivo, no te permitia. Ahora tambien chequea que no sea el origen) Arregle el metodo ShowAbleToAttack para que te permita atacar a las unidades en una celda compartida. Agarraba los vecinos, pero nunca preguntaba por la celda en la que estoy.

--- CrystalClash/CrystalClash/src/pruebas/Renders/NormalGame.java
+++ CrystalClash/CrystalClash/src/pruebas/Renders/NormalGame.java
@@ -44,10 +44,6 @@ import com.badlogic.gdx.utils.Array;
 public class NormalGame extends GameRender {
 	private TweenManager tweenManager;
 
-	// private Unit testUnit1;
-	// private Unit testUnit2;
-	// private Unit testUnit3;
-	// private Unit testUnit4;
 	private Unit selectedUnit;
 	private Cell selectedCell;
 
@@ -111,35 +107,6 @@ public class NormalGame extends GameRender {
 
 	public void init() {
 		GameController.getInstancia().loadUnitsStats();
-
-		// testUnit1 = new Unit("fire_archer");
-		// if (world.player == 2)
-		// testUnit1.getRender().setFacing(FACING.left);
-		//
-		// testUnit2 = new Unit("earth_tank");
-		// if (world.player == 2)
-		// testUnit2.getRender().setFacing(FACING.left);
-		//
-		// testUnit3 = new Unit("darkness_mage");
-		// if (world.player == 2)
-		// testUnit3.getRender().setFacing(FACING.left);
-		//
-		// testUnit4 = new Unit("wind_assassin");
-		// if (world.player == 2)
-		// testUnit4.getRender().setFacing(FACING.left);
-		//
-		// world.setCellState(165, 675, Cell.State.ABLE_TO_PLACE);
-		// world.placeUnit(165, 690, testUnit1);
-		// world.setCellState(165, 675, Cell.State.NONE);
-		// world.setCellState(635, 675, Cell.State.ABLE_TO_PLACE);
-		// world.placeUnit(635, 675, testUnit2);
-		// world.setCellState(635, 675, Cell.State.NONE);
-		// world.setCellState(1140, 675, Cell.State.ABLE_TO_PLACE);
-		// world.placeUnit(1140, 675, testUnit3);
-		// world.setCellState(1140, 675, Cell.State.NONE);
-		// world.setCellState(550, 450, Cell.State.ABLE_TO_PLACE);
-		// world.placeUnit(550, 450, testUnit4);
-		// world.setCellState(550, 450, Cell.State.NONE);
 
 		Texture arrow = new Texture(
 				Gdx.files.internal("data/Images/InGame/selector_arrow.png"));
@@ -334,6 +301,9 @@ public class NormalGame extends GameRender {
 			int enemyPlayer = world.player == 1 ? 2 : 1;
 			boolean continueMoving = top.getUnit(enemyPlayer) == null;
 
+			if(top.Equals(unitAction.origin))
+				continueMoving = true;
+			
 			if (continueMoving) {
 				int[][] cells = top.neigbours;
 				Cell aux = null;
@@ -359,13 +329,16 @@ public class NormalGame extends GameRender {
 		showAbleToAttackCellRecirsive(selectedCell, checkIfUnit, selectedUnit.getRange(), false);
 	}
 
-	// Method that actually "shows" (change state) the cell where units can
-	// attack
+	// Method that actually "shows" (change state) the cell where units can attack
 	private void showAbleToAttackCellRecirsive(Cell cell, boolean checkIfUnit, int range, boolean hide) {
 		int[][] cells = cell.neigbours;
+		int enemyPlayer = world.player == 1 ? 2 : 1;
+		
+		if(checkIfUnit && cell.getUnit(enemyPlayer) != null)
+			cell.setState(Cell.State.ABLE_TO_ATTACK);
+			
 		Cell aux = null;
-		for (int i = 0; i < cell.neigbours.length; i++) {
-			int enemyPlayer = world.player == 1 ? 2 : 1;
+		for (int i = 0; i < cells.length; i++) {
 			aux = world.cellAtByGrid(cells[i][0], cells[i][1]);
 
 			if ((checkIfUnit && aux.getUnit(enemyPlayer) == null) || hide)
