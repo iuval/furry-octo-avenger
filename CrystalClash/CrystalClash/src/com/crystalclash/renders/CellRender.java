f209425b179db5be36bcb7d5cce538ada7162653
iuval <iuvalgoldansky@gmail.com>
2013-11-01 19:34:32 -0200

    Agrega el auto camino

--- CrystalClash/CrystalClash/src/com/crystalclash/renders/CellRender.java
+++ CrystalClash/CrystalClash/src/com/crystalclash/renders/CellRender.java
@@ -1,9 +1,10 @@
 package com.crystalclash.renders;
 
-
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.crystalclash.entities.Cell;
 import com.crystalclash.entities.Unit;
+import com.crystalclash.renders.helpers.ResourceHelper;
 import com.crystalclash.views.WorldView;
 
 public class CellRender {
@@ -13,8 +14,11 @@ public class CellRender {
 		this.cell = cell;
 	}
 
+	BitmapFont font = ResourceHelper.getFont();
+
 	public void draw(float dt, SpriteBatch batch) {
 		WorldView.cellHelper.drawCellTextures(batch, cell);
+		font.draw(batch, cell.getGridPosition().getX() + ", " + cell.getGridPosition().getY(), cell.getX(), cell.getCenterY());
 	}
 
 	public void drawUnits(float dt, SpriteBatch batch) {
